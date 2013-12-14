98dc3444a5d662d0fc57f25f5b22426a9550fb30
thefallen28 <dawnofffallen@gmail.com>
2013-12-14 16:58:28 +0530

    Create TEST.java

---
+++ TEST.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TEST
+{
+
+	public static void main(String args[])
+	{
+		int i;
+		Scanner in = new Scanner(System.in);
+		while(true)
+		{
+		i = in.nextInt();
+		if(i==42)
+			break;
+			System.out.println(i);
+	}
+		}
+}
